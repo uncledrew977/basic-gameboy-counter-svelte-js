735eb94caacf0f1f4768b81809cd9f205f244a3b
Andrew <95650480+uncledrew977@users.noreply.github.com>
2025-05-17 02:05:11 -0400

    Update Paste.ps1

--- Paste.ps1
+++ Paste.ps1
@@ -1,6 +1,14 @@
+$htmlBytes = Get-Content $htmlPath -Encoding Byte
+$decodedContent = [System.Text.Encoding]::GetEncoding("windows-1252").GetString($htmlBytes)
+
+# Nuke all comments
 $decodedContent = [regex]::Replace(
     $decodedContent,
-    '<\s*script\b[^>]*>[\s\S]*?<\s*/\s*script\s*>',
+    '<!--[\s\S]*?-->',
     '',
     [System.Text.RegularExpressions.RegexOptions]::IgnoreCase
 )
+
+# Now load clean content
+$doc = New-Object HtmlAgilityPack.HtmlDocument
+$doc.LoadHtml($decodedContent)
