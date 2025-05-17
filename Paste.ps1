191cad14a9fe5d2ea9d69bb36fe7005df1502c0e
Andrew <95650480+uncledrew977@users.noreply.github.com>
2025-05-17 01:56:37 -0400

    Update Paste.ps1

--- Paste.ps1
+++ Paste.ps1
@@ -1,13 +1,1 @@
-$headNode = $doc.DocumentNode.SelectSingleNode("//head")
-if ($headNode -ne $null) {
-    foreach ($child in @($headNode.ChildNodes)) {
-        $text = $child.InnerHtml
-
-        if ($text -match '<script[^>]*language\s*=\s*["'']?javascript["'']?[^>]*>.*?</script>' -or
-            $text -match 'function\s+\w+\s*\(' -or
-            $text -match '<!--\s*function') {
-
-            $headNode.RemoveChild($child)
-        }
-    }
-}
+$decodedContent = $decodedContent -replace '<script[^>]*language\s*=\s*["'']?javascript["'']?[^>]*>.*?<\/script>', '', 'Singleline,IgnoreCase'
