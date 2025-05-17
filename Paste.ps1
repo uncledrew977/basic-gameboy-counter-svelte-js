95ff556526410ae9f1f38db861cbb980f4c7a6a4
Andrew <95650480+uncledrew977@users.noreply.github.com>
2025-05-17 02:01:20 -0400

    Update Paste.ps1

--- Paste.ps1
+++ Paste.ps1
@@ -1,6 +1,7 @@
+# Strip all <script>...</script> blocks from raw HTML
 $decodedContent = [regex]::Replace(
     $decodedContent,
-    '<script[^>]*language\s*=\s*["'']?javascript["'']?[^>]*>.*?</script>',
+    '<script[^>]*>.*?</script\s*>',
     '',
     [System.Text.RegularExpressions.RegexOptions]::Singleline -bor [System.Text.RegularExpressions.RegexOptions]::IgnoreCase
 )
